ea97f01293ec019ec33fd12e3fe20645e88e7494
ChangZhehao <changzhehao@gmail.com>
2019-05-22 00:38:59 +1000

    generate the structure of generateUUID()

---
+++ src/main/java/util/UUIDUtils.java
@@ -0,0 +1,20 @@
+package util;
+
+/**
+ * User: u6613739
+ * Date: 2019/5/22
+ * Time: 0:36
+ * Description:
+ */
+public class UUIDUtils
+{
+    /**
+     * generate a UUID
+     * @return
+     */
+    public static long generateUUID()
+    {
+        //TODO: 嘉嘉写一个 初始化一个全局唯一的ID，叫做UUID
+        return -1;
+    }
+}
