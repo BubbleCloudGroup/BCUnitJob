5e301ec3a1c3e8a646db489dc2ed78e98381cce1
ZHANG Xujia <zxj2014302567@outlook.com>
2019-05-27 20:40:08 +1000

    finish random ID generation function.

--- src/main/java/util/UUIDUtils.java
+++ src/main/java/util/UUIDUtils.java
@@ -1,20 +1,54 @@
 package util;
 
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 /**
  * User: u6613739
  * Date: 2019/5/22
  * Time: 0:36
  * Description:
  */
-public class UUIDUtils
-{
+public class UUIDUtils {
+
+
+    private static  String hostTimeLast="";
+    private static int sameTimeNumber=0;
+
     /**
      * generate a UUID
-     * @return
+     *
+     * @return BigInteger
      */
-    public static long generateUUID()
-    {
-        //TODO: 嘉嘉写一个 初始化一个全局唯一的ID，叫做UUID
-        return -1;
+    public synchronized static BigInteger generateUUID() {
+        String uuidString="";
+        try{
+            String hostAddress=InetAddress.getLocalHost().getHostAddress();
+            String hostTime=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            if(hostTimeLast.equals(hostTime)) {
+                sameTimeNumber++;
+            }
+            else {
+                sameTimeNumber=0;
+                hostTimeLast=hostTime;
+            }
+            uuidString=hostAddress+hostTime+sameTimeNumber;
+            uuidString=uuidString.replaceAll("[^0-9]", "");
+        }catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return new BigInteger(uuidString,10);
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
     }
 }
