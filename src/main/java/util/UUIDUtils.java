bfabce9ec6fad56597b8696e3afe2ea5fb3a5584
ZHANG Xujia <zxj2014302567@outlook.com>
2019-05-31 21:50:21 +1000

    modify random ID generation function.

--- src/main/java/util/UUIDUtils.java
+++ src/main/java/util/UUIDUtils.java
@@ -1,10 +1,9 @@
 package util;
 
-import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
+
 
 
 /**
@@ -16,32 +15,71 @@ import java.util.Date;
 public class UUIDUtils {
 
 
-    private static  String hostTimeLast="";
+    private static  long hostTimeLast=0;
     private static int sameTimeNumber=0;
+    private static String hostAddress;
+
+    static {
+        try {
+            String tempHostAddress = InetAddress.getLocalHost().getHostAddress();
+            int pos1 = tempHostAddress.indexOf(".");
+            int pos2 = tempHostAddress.indexOf(".", pos1 + 1);
+            int pos3 = tempHostAddress.indexOf(".", pos2 + 1);
+
+            int number;
+            number = tempHostAddress.substring(0, pos1).length();
+            if (number == 1) {
+                hostAddress = "00" + tempHostAddress.substring(0, pos1);
+            } else if (number== 2) {
+                hostAddress = "0" + tempHostAddress.substring(0, pos1);
+            } else {
+                hostAddress = tempHostAddress.substring(0, pos1);
+            }
+            number = tempHostAddress.substring(pos1 + 1, pos2).length();
+            if (number == 1) {
+                hostAddress = hostAddress + "00" + tempHostAddress.substring(pos1 + 1, pos2);
+            } else if (number == 2) {
+                hostAddress = hostAddress + "0" + tempHostAddress.substring(pos1 + 1, pos2);
+            } else {
+                hostAddress = hostAddress + tempHostAddress.substring(pos1 + 1, pos2);
+            }
+            number = tempHostAddress.substring(pos2 + 1, pos3).length();
+            if (number == 1) {
+                hostAddress = hostAddress + "00" + tempHostAddress.substring(pos2 + 1, pos3);
+            } else if (number == 2) {
+                hostAddress = hostAddress + "0" + tempHostAddress.substring(pos2 + 1, pos3);
+            } else {
+                hostAddress = hostAddress + tempHostAddress.substring(pos2 + 1, pos3);
+            }
+            number = tempHostAddress.length() - (pos3 + 1);
+            if (number == 1) {
+                hostAddress = hostAddress + "00" + tempHostAddress.substring(pos3 + 1);
+            } else if (number == 2) {
+                hostAddress = hostAddress + "0" + tempHostAddress.substring(pos3 + 1);
+            } else {
+                hostAddress = hostAddress + tempHostAddress.substring(pos3 + 1);
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * generate a UUID
      *
-     * @return BigInteger
+     * @return String
      */
-    public synchronized static BigInteger generateUUID() {
-        String uuidString="";
-        try{
-            String hostAddress=InetAddress.getLocalHost().getHostAddress();
-            String hostTime=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-            if(hostTimeLast.equals(hostTime)) {
-                sameTimeNumber++;
-            }
-            else {
-                sameTimeNumber=0;
-                hostTimeLast=hostTime;
-            }
-            uuidString=hostAddress+hostTime+sameTimeNumber;
-            uuidString=uuidString.replaceAll("[^0-9]", "");
-        }catch (UnknownHostException e) {
-            e.printStackTrace();
+    public synchronized static String generateUUID() {
+        String uuidString;
+        long fastTime = Calendar.getInstance().getTimeInMillis();
+        if (hostTimeLast == fastTime) {
+            sameTimeNumber++;
+        } else {
+            sameTimeNumber = 0;
+            hostTimeLast = fastTime;
         }
-        return new BigInteger(uuidString,10);
+        uuidString = hostAddress + fastTime + sameTimeNumber;
+        return uuidString;
     }
 
     /**
